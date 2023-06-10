e1c49c5cdd30b9e9da340b66ca7a62be40eedcf5
Ervin Pangilinan <pangilinanervin22@gmail.com>
2023-06-11 00:32:04 +0800

    add: signup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 import Sample from './components/common/Sample'
-import './styles/App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './api'
 import Root from './pages/Root'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
 
@@ -17,7 +18,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<SignUp />} />
           <Route path='/' element={<Root />}>
+
             <Route path='/sample' element={<Sample message='sample' />} />
             <Route path='/wew' element={<Sample message='wew' />} />
             <Route path='*' element={<div>Not Found</div>} />
