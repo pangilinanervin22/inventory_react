4ef819eba88c8404ebbc220904ba7d536a67aa08
Ervin Pangilinan <pangilinanervin22@gmail.com>
2023-07-08 22:39:20 +0800

    add: state management of token and user information

--- src/app/login.ts
+++ src/app/login.ts
@@ -1,15 +1,37 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type AuthStore = {
+    img_src: string,
+    position: string,
     isAuthenticated: boolean;
-    login: () => void;
+    token: "",
+    login: (token: any) => void;
     logout: () => void;
+    changeCredentials: (value: any) => void;
 };
 
-const useAuthStore = create<AuthStore>((set) => ({
-    isAuthenticated: false,
-    login: () => set({ isAuthenticated: true }),
-    logout: () => set({ isAuthenticated: false }),
-}));
+const storeUserProfile = create<AuthStore>()(
+    persist(
+        (set) => ({
+            isAuthenticated: false,
+            img_src: "",
+            token: "",
+            position: "guest",
+            login: (token: any) => set({ token: token }),
+            logout: () => set({ token: "" }),
+            changeCredentials: (value: any) =>
+                set({
+                    position: value.position,
+                    img_src: value.img_src,
+                    token: value.token,
+                }),
+        }),
+        {
+            name: "login", // unique name
+            storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+        }
+    )
+);
 
-export default useAuthStore;
+export default storeUserProfile;
